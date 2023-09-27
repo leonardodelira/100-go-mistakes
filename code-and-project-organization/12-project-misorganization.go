483a1653c7077733a7d6dfe649755d6a53020927
leonardodelira <leonardo.dlira@mercadolivre.com>
2023-09-27 11:50:45 -0300

    mistake 12

---
+++ code-and-project-organization/12-project-misorganization.go
@@ -0,0 +1,7 @@
+package codeandprojectorganization
+
+/*
+Project structure
+Existe uma convenção não oficial de como organizar códigos GO:
+Mais detalhes nesse github (https://github.com/golang-standards/project-layout).
+*/
